dc466974d7672df5b6525511f149a32fee49cdfd
Alexander Ruziska <ziskapants@gmail.com>
2024-11-24 22:14:13 -0600

    Final

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -135,14 +135,16 @@ console.log(gems);
 //       "Emerald and Diamond and Tourmaline and Topaz"
 //     🔥 You'll need to research the .join Array method. 🔥
 //     Don't forget to console.log `gemString`...
-
+let gemString = gems.join(' and ');
+console.log(gemString);
 
 // 5.d (STRETCH) TODO: Make a new array `colorfulGems` that combines 
 //     the gems array with the colors array.
 //     It should look something like:
 //       ['Tourmaline', 'Topaz', 'Tanzanite', 'Iolite', 'Emerald', 'Diamond', 'yellow', 'green']
 //     Now, console.log the new array.
-
+let colorfulGems = gems.concat(colors);
+console.log(colorfulGems);
 
 
 
